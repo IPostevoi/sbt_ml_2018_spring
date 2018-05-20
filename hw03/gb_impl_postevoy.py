5d04123b479c8e98056f47c856480248aee599c7
IPostevoi <baklandor410@yandex.ru>
2018-05-21 01:45:03 +0300

    +hw3

--- hw03/gb_impl_postevoy.py
+++ hw03/gb_impl_postevoy.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 # Параметрами с которыми вы хотите обучать деревья
-TREE_PARAMS_DICT = {'max_depth': 3}
+TREE_PARAMS_DICT = {'max_depth': 4, 'random_state': 10}
 # Параметр tau (learning_rate) для вашего GB
 TAU = 0.05
 
@@ -18,25 +18,19 @@ class SimpleGB(BaseEstimator):
         self.tau = tau
         
     def fit(self, X_data, y_data):
+        y_data = 2 * y_data - 1
         self.base_algo = DecisionTreeRegressor(**self.tree_params_dict).fit(X_data, y_data)
         self.estimators = []
         curr_pred = self.base_algo.predict(X_data)
-        for iter_num in range(self.iters):
-            # Нужно посчитать градиент функции потерь
-            grad = y_data / (1 + np.exp(y_data * curr_pred))
-            # Нужно обучить DecisionTreeRegressor предсказывать антиградиент
-            # Не забудьте про self.tree_params_dict
-            algo = DecisionTreeRegressor().fit(X_data, y_data) # TODO
-
+        for i in range(self.iters):
+            grad = y_data / (1 + np.exp(-2 * y_data * curr_pred))
+            algo = DecisionTreeRegressor().fit(X_data, grad)
             self.estimators.append(algo)
-            # Обновите предсказания в каждой точке
             curr_pred += self.tau * algo.predict(X_data)
         return self
     
     def predict(self, X_data):
-        # Предсказание на данных
         res = self.base_algo.predict(X_data)
         for estimator in self.estimators:
             res += self.tau * estimator.predict(X_data)
-        # Задача классификации, поэтому надо отдавать 0 и 1
-        return res > -0.1
+        return res > 0.
